ce46e263069ccd63bd79eae74a652f9b813f8e1c
Virgil <13421565@student.uts.edu.au>
2021-05-17 14:40:32 +1000

    add css

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,6 +7,7 @@ const BookingController = require('../controller/Booking')
 const AuthController = require('../controller/Authorization')
 const initializePassport = require('../controller/passport')
 const Booking = require('../model/Booking');
+const Customer = require('../model/Customer')
 const { ReplSet } = require('mongodb');
 
 initializePassport(
@@ -16,14 +17,41 @@ initializePassport(
 )
 
 //grabbing info from post of register html page to make database
-router.post('/register', AuthController.register)
+router.post('/register', (req,res,next) =>{
+ const { firstName, lastName, email, password, password2 } = req.body;
+let errors = [];
+
+if (!firstName || !lastName || !email || !password || !password2) {
+  errors.push({ msg: 'Please enter all fields' });
+}
+
+if (password != password2) {
+  errors.push({ msg: 'Passwords do not match' });
+}
+
+if (password.length < 8) {
+  errors.push({ msg: 'Password must be at least 6 characters' });
+}
+
+if (errors.length > 0) {
+  res.render('register', {
+    errors,
+    firstName,
+    lastName,
+    email,
+    password,
+    password2
+  });
+}
+next()},AuthController.register)
 
 router.get('/login',(req,res)=>{
   res.render('login')
 });
 
 //redirect to register 
-router.get('/register',(req,res)=>{
+router.get('/register', 
+  (req,res)=>{
   res.render('register')
 });
 
@@ -35,7 +63,7 @@ router.get('/menu',(req,res)=>{
   res.render('menu')
 });
 
-router.get('/changeBooking',(req,res,next) =>{
+router.get('/changeBooking',AuthController.ensureAdminAuthenticated,(req,res,next) =>{
   //Here fetch data using mongoose query like
   Booking.find({}, function(err, data) {
   if (err) throw err;
@@ -49,11 +77,98 @@ router.get('/userBooking', AuthController.ensureAuthenticated, (req,res,next) =>
   Booking.find({email: req.user.email}, function(err, data) {
   if (err) throw err;
   // object of all the users
-  res.render('changeBooking',{Booking:data});
+  res.render('userBooking',{Booking:data});
+}).sort({"date":1})
+});
+
+router.get('/changeUser', AuthController.ensureAdminAuthenticated, (req,res,next) =>{
+  //Here fetch data using mongoose query like
+  Customer.find({}, function(err, data) {
+  if (err) throw err;
+  // object of all the users
+  res.render('adminUserInfo',{Customer:data});
+}).sort({"date":1})
+});
+
+
+router.get('/userInfo', AuthController.ensureAuthenticated, (req,res,next) =>{
+  //Here fetch data using mongoose query like
+  Customer.find({email: req.user.email}, function(err, data) {
+  if (err) throw err;
+  // object of all the users
+  res.render('userInfo',{Customer:data});
 }).sort({"date":1})
 });
 
 router.get('/edit/:id',AuthController.ensureAuthenticated, (req,res) =>{
+  var editId = req.params.id
+  userData = Booking.findById(editId)
+  userData.exec(function(err,data){
+    if(err) throw err
+    res.render('editBooking',{userData:data})
+  })
+}
+)
+
+router.get('/profileEdit/:id',AuthController.ensureAuthenticated, (req,res) =>{
+  var editId = req.params.id
+  userData = Customer.findById(editId)
+  userData.exec(function(err,data){
+    if(err) throw err
+    res.render('editUserInfo',{userData:data})
+  })
+}
+)
+
+router.post('/profileEdit/:id',AuthController.ensureAuthenticated,(req,res) => {
+  var inputData = req.body
+  var editId = req.params.id
+  userData = Customer.findByIdAndUpdate(editId, inputData)
+  userData.exec(function(err,data){
+    if(err) throw err;
+    req.flash("success_msg","User profile successfully updated")
+    res.redirect('/users/userInfo')
+  })
+}
+)
+
+router.get('/userEdit/:id',AuthController.ensureAdminAuthenticated, (req,res) =>{
+  var editId = req.params.id
+  userData = Customer.findById(editId)
+  userData.exec(function(err,data){
+    if(err) throw err
+    res.render('editUserInfo',{userData:data})
+  })
+}
+)
+
+router.post('/userEdit/:id',AuthController.ensureAdminAuthenticated,(req,res) => {
+  var inputData = req.body
+  var editId = req.params.id
+  userData = Customer.findByIdAndUpdate(editId, inputData)
+  userData.exec(function(err,data){
+    if(err) throw err;
+    req.flash("success_msg","User profile successfully updated")
+    res.redirect('/users/changeUser')
+  })
+}
+)
+
+router.get('/userDelete/:id', AuthController.ensureAdminAuthenticated, (req,res,next) => {
+  var deleteId = req.params.id
+  userData = Customer.findByIdAndDelete(deleteId)
+  userData.exec(function(err,data){
+    if(err) throw err
+  })
+  next()
+},(req,res)=>{
+  req.flash("success_msg", "User deleted successfully")
+  res.redirect('/users/changeUser')
+}
+)
+
+
+router.get('/adminEdit/:id',AuthController.ensureAdminAuthenticated, (req,res) =>{
   var editId = req.params.id
   userData = Booking.findById(editId)
   userData.exec(function(err,data){
@@ -74,9 +189,11 @@ router.get('/delete/:id', AuthController.ensureAuthenticated, (req,res,next) => 
   next()
 },(req,res)=>{
   req.flash("success_msg", "Booking deleted successfully")
-  res.redirect('/users/dashboard')
+  res.redirect('/users/userBooking')
 }
 )
+
+
 
 router.get('/logout', (req, res) => {
   req.logout();
@@ -97,6 +214,30 @@ router.post('/edit/:id',AuthController.ensureAuthenticated,(req,res) => {
 }
 )
 
+router.post('/adminEdit/:id',AuthController.ensureAdminAuthenticated,(req,res) => {
+  var inputData = req.body
+  var editId = req.params.id
+  userData = Booking.findByIdAndUpdate(editId, inputData)
+  userData.exec(function(err,data){
+    if(err) throw err;
+    req.flash("success_msg","Booking successfully editted")
+    res.redirect('/users/changeBooking')
+  })
+}
+)
+
+router.get('/adminDelete/:id', AuthController.ensureAdminAuthenticated, (req,res,next) => {
+  var deleteId = req.params.id
+  userData = Booking.findByIdAndDelete(deleteId)
+  userData.exec(function(err,data){
+    if(err) throw err
+  })
+  next()
+},(req,res)=>{
+  req.flash("success_msg", "Booking deleted successfully")
+  res.redirect('/users/changeBooking')
+}
+)
 
 
 router.post('/login', (req, res, next) => {
@@ -113,10 +254,21 @@ router.get('/dashboard', AuthController.ensureAuthenticated, (req, res) =>
   })
 );
 
-router.get('/adminLogin', (req, res) =>
+router.get('/adminLogin',(req,res) => {
   res.render('adminLogin')
-);
+})
 
+router.post('/adminLogin' ,AuthController.isAdmin, (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/users/adminDashboard',
+    failureRedirect: '/users/adminLogin',
+    failureFlash: true
+  })(req, res, next);
+});
+
+router.get('/adminDashboard', AuthController.ensureAdminAuthenticated, (req,res) => {
+  res.render('adminDashboard',{user:req.user})
+})
 
 router.post('/booking', BookingController.book)
 
