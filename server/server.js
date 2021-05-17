27f9860346d3883d7dcf2f9e9659c4feeb9036af
AndLin-hub <13623905@student.uts.edu.au>
2021-05-17 14:08:06 +1000

    proper css handling in ejs

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.use((req, res, next) =>{
 app.use(express.urlencoded({extended: false})); 
 
 //css for the register page
-app.use(express.static(__dirname +'/css'))
+app.use(express.static(__dirname +'/public'))
 
 
 app.get('/',(req,res)=>{
